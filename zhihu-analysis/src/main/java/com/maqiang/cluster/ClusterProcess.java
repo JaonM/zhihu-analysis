6c221615955fb3ac58a3f8f4ffb63bc1d869adc0
马强 <maqiang@maqiangs-MacBook-Pro.local>
2017-03-13 22:36:12 +0800

    更新聚类过程

--- zhihu-analysis/src/main/java/com/maqiang/cluster/ClusterProcess.java
+++ zhihu-analysis/src/main/java/com/maqiang/cluster/ClusterProcess.java
@@ -10,9 +10,8 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +19,29 @@ import java.util.List;
  * Created by maqiang on 05/03/2017.
  */
 public class ClusterProcess {
+
+    public static void main(String [] args) {
+        try {
+            Configuration conf = new Configuration();
+            FileSystem fs = FileSystem.get(URI.create("hdfs://localhost:9000"),conf);
+            Path path = new Path("hdfs://localhost:9000/zhihu_mining/cluster/result/result.txt");
+            if(fs.exists(path)) {
+                fs.createNewFile(path);
+            }
+            List<Cluster> clusters = process(conf,fs);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fs.create(path)));
+            for(Cluster cluster:clusters) {
+                for(UserNode userNode:cluster.getUserNodes()) {
+                    bw.write(userNode.getName()+"\t");
+                }
+                bw.write("\n");
+            }
+            bw.flush();
+            bw.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static List<Cluster> process(Configuration conf, FileSystem fs) throws IOException {
         List<Neuron> neurons = NeuronUtil.loadNeuronFromFile(conf, fs, NeuronUtil.WEIGHT_PATH);
